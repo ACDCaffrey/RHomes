810bdf821341bacf7990626983cfe779e2ebacdb
Adam Caffrey <adam.d.caffrey@gmail.com>
2022-09-11 17:05:39 -0700

    added map render functions

--- RHomes/server.R
+++ RHomes/server.R
@@ -4,10 +4,20 @@ library(shiny)
 shinyServer(function(input, output, sessions) {
 
   output$ukmap <- renderLeaflet({
-    leaflet() %>% 
-      addProviderTiles(providers$Esri.WorldImagery) %>% 
-      setView(lat = 54.347028, lng = -2.133141, 
-              zoom = 6)
+    
+    if(input$render_map == 0){return()}
+    
+    isolate({
+    
+      map_style <- input$select_map
+      
+      leaflet() %>% 
+        addProviderTiles(map_style) %>% 
+        setView(lat = 54.347028, lng = -2.133141, 
+                zoom = 6)
+      
+    })
+    
   })
   
 })
